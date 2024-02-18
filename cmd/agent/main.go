4def9caec28ce21e5448ace1fc230c08cad4b559
Smer4k <151877040+Smer4k@users.noreply.github.com>
2024-02-18 15:40:04 +0300

    update agent\main.go
    добавлены пару проверок

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/Smer4k/slow-web-calculator/internal/agent"
@@ -21,6 +23,13 @@ func init() {
 func main() {
 	if strings.Contains(port, ":") {
 		panic("Один из параметров содержит \":\". В параметрах запуска не нужно указывать двоеточие")
+	}
+	re := regexp.MustCompile(`^[0-9]+$`)
+	if !re.MatchString(port) {
+		panic("Порт должен содержать только цифры")
+	}
+	if num, _ := strconv.Atoi(port); num > 65535 {
+		panic("Порт не должен быть больше 65535")
 	}
 	port = ":" + port
 	orchestratorPort := ":8080"
